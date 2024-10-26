334ffbc8abf3ef64c5195bf7907da793c66e4445
Loïc Corenthy <loic.corenthy@gmail.com>
2024-10-26 14:43:49 +0100

    Add benchmark for Queue2

---
+++ Utilities/tests/src/BenchmarkTSQueue2.cpp
@@ -0,0 +1,91 @@
+#include "Helper.hpp"
+#include "BenchmarkFixture.hpp"
+
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/benchmark/catch_benchmark.hpp>
+
+#include <thread>
+
+using LCNS::ThreadSafe::Queue2;
+
+using std::atomic;
+using std::jthread;
+
+TEST_CASE_METHOD(BenchmarkFixture, "1 consumer - 5 producers", "[benchmark][single_consumer]")
+{
+    BENCHMARK("1 consumers - 5 producers")
+    {
+        Queue2<int> tsq2;
+
+        auto consumer = [](Queue2<int>& queue)
+        {
+            int tested_items = 0;
+
+            while (tested_items < total_item_count)
+            {
+                int value = std::numeric_limits<int>::max();
+                if (queue.try_pop(value))
+                {
+                    CHECK((-item_per_thread_count <= value && value <= item_per_thread_count));
+                    tested_items++;
+                }
+            }
+        };
+
+        jthread consumer_thread(consumer, std::ref(tsq2));
+
+        jthread producers[input_thread_count];
+
+        for (int i = 0; i < input_thread_count; ++i)
+        {
+            producers[i] = jthread(producer2, std::ref(tsq2), item_per_thread_count);
+        }
+
+        consumer_thread.join();
+        CHECK(tsq2.is_empty());
+        return 0;
+    };
+}
+
+TEST_CASE_METHOD(BenchmarkFixture, "5 consumers - 5 producers", "[benchmark][multi_consumers]")
+{
+    BENCHMARK("5 consumers - 5 producers")
+    {
+        Queue2<int> tsq2;
+        atomic<int> all_tested_items = 0;
+
+        auto consumer = [](Queue2<int>& queue, std::atomic<int>& all_tested_items)
+        {
+            // Probably not the best condition but it works for now
+            while (all_tested_items < total_item_count)
+            {
+                int value = std::numeric_limits<int>::max();
+                if (queue.try_pop(value))
+                {
+                    CHECK((-total_item_count <= value && value <= total_item_count));
+                    all_tested_items++;
+                }
+            }
+        };
+
+        {
+            jthread consumers[input_thread_count];
+
+            for (int i = 0; i < input_thread_count; ++i)
+            {
+                consumers[i] = jthread(consumer, std::ref(tsq2), std::ref(all_tested_items));
+            }
+
+            jthread producers[input_thread_count];
+
+            for (int i = 0; i < input_thread_count; ++i)
+            {
+                producers[i] = jthread(producer2, std::ref(tsq2), item_per_thread_count);
+            }
+        }
+
+        CHECK(tsq2.is_empty());
+
+        return 0;
+    };
+}
