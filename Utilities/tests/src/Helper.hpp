0391811f80b26d85baa23ad54f756641d63f2bf9
Loïc Corenthy <loic.corenthy@gmail.com>
2024-11-08 15:46:00 +0000

    Minor readability update

--- Utilities/tests/src/Helper.hpp
+++ Utilities/tests/src/Helper.hpp
@@ -29,39 +29,39 @@ struct TestData
     explicit TestData(std::string init_name)
     : name(std::move(init_name))
     {
-        std::printf("Test Data: constructor with name %s\n", name.c_str());
+        std::printf("Test Data: constructor with name \"%s\"\n", name.c_str());
     }
 
     TestData(const TestData& rhs)
     : name(rhs.name)
     {
-        std::printf("Test Data: copy constructor with name  %s\n", name.c_str());
+        std::printf("Test Data: copy constructor with name  \"%s\"\n", name.c_str());
     }
 
     TestData(TestData&& rhs)
     : name(std::move(rhs.name))
     {
-        std::printf("Test Data: move constructor  %s\n", name.c_str());
+        std::printf("Test Data: move constructor  \"%s\"\n", name.c_str());
     }
 
     TestData& operator=(const TestData& rhs)
     {
         name = rhs.name;
-        std::printf("Test Data: copy assignment operator  %s\n", name.c_str());
+        std::printf("Test Data: copy assignment operator  \"%s\"\n", name.c_str());
         return *this;
     }
 
     TestData& operator=(TestData&& rhs)
     {
         name = std::move(rhs.name);
-        std::printf("Test Data: move assignment operator  %s\n", name.c_str());
+        std::printf("Test Data: move assignment operator  \"%s\"\n", name.c_str());
 
         return *this;
     }
 
     ~TestData()
     {
-        std::printf("Test Data: Destructor with name %s\n", name.c_str());
+        std::printf("Test Data: Destructor with name \"%s\"\n", name.c_str());
     }
 
     std::string name;
