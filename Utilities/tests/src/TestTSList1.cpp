fd4dc578e170e45887105880ca4f301fe033f953
Loïc Corenthy <loic.corenthy@gmail.com>
2024-11-11 16:31:19 +0000

    Refactor internal List1 tests

--- Utilities/tests/src/TestTSList1.cpp
+++ Utilities/tests/src/TestTSList1.cpp
@@ -3,15 +3,84 @@
 #include "Helper.hpp"
 
 #include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators_random.hpp>
 
 #include <thread>
 #include <iostream>
 
+using Catch::Generators::random;
 using LCNS::ThreadSafe::List1;
 
-TEST_CASE("Empty list", "[test][internal]")
+class TestList1Fixture
+{
+public:
+    static bool always_match([[maybe_unused]] int i)
+    {
+        return true;
+    }
+
+    static bool match_one(const int i)
+    {
+        return i == 1;
+    };
+
+    static bool match_two(const int i)
+    {
+        return i == 2;
+    };
+
+    static bool match_three(const int i)
+    {
+        return i == 3;
+    };
+};
+
+TEST_CASE_METHOD(TestList1Fixture, "Empty list", "[test][internal]")
 {
     GIVEN("An empty list")
+    {
+        List1<int> list;
+
+        WHEN("Trying to get any element")
+        {
+            const auto result = list.find_first_if(TestList1Fixture::always_match);
+
+            THEN("The list returns nullptr")
+            {
+                CHECK(result == nullptr);
+            }
+        }
+    }
+
+    GIVEN("A list with added elements")
+    {
+        List1<int> list;
+
+        const auto count = random(10, 100).get();
+        for (int i = 0; i < count; ++i)
+        {
+            list.push_front(random(-1000, 1000).get());
+        }
+
+        WHEN("All the elements are removed")
+        {
+            for (int i = 0; i < count; ++i)
+            {
+                list.remove_if(TestList1Fixture::always_match);
+            }
+
+            THEN("The list returns nullptr")
+            {
+                const auto result = list.find_first_if(TestList1Fixture::always_match);
+                CHECK(result == nullptr);
+            }
+        }
+    }
+}
+
+TEST_CASE_METHOD(TestList1Fixture, "Adding elements", "[test][internal]")
+{
+    GIVEN("A list with known added elements")
     {
         List1<int> list;
 
@@ -19,28 +88,103 @@ TEST_CASE("Empty list", "[test][internal]")
         list.push_front(2);
         list.push_front(3);
 
-        auto predicate = [](int i) { return i % 2 == 0 || i % 2 == 1; };
+        WHEN("They are retrieved")
+        {
+            const auto one   = list.find_first_if(TestList1Fixture::match_one);
+            const auto two   = list.find_first_if(TestList1Fixture::match_two);
+            const auto three = list.find_first_if(TestList1Fixture::match_three);
 
-        list.remove_if(predicate);
+            THEN("They match the original values")
+            {
+                CHECK(*one == 1);
+                CHECK(*two == 2);
+                CHECK(*three == 3);
+            }
+        }
 
-        auto my_print = [](int i) { std::cout << i << '\n'; };
-        auto my_const_print = [](const int& i) { std::cout << i << '\n'; };
-        auto multiply_by_two = [](int& i) { i *= 2; };
+        WHEN("Looking for a value not in the list")
+        {
+            const auto match_four = [](const int i) { return i == 4; };
+            const auto four       = list.find_first_if(match_four);
 
-        list.push_front(4);
-        list.push_front(5);
-        list.push_front(6);
+            THEN("The list returns nullptr")
+            {
+                CHECK(four == nullptr);
+            }
+        }
+    }
+}
 
-        list.for_each(my_print);
-        list.for_each(multiply_by_two);
-        list.for_each(my_const_print);
+TEST_CASE_METHOD(TestList1Fixture, "Modifying elements", "[test][internal]")
+{
+    GIVEN("A list with known added elements")
+    {
+        List1<int> list;
 
-        auto divisible_by_2 = [](int i ) { return i % 2 == 0;};
-        auto res = list.find_first_if(divisible_by_2);
-        CHECK(*res == 12);
+        list.push_front(1);
+        list.push_front(2);
+        list.push_front(3);
 
-        auto more_than_20 = [](int i ) { return i > 20;};
-        res = list.find_first_if(more_than_20);
-        CHECK(!res);
+        WHEN("They are modified")
+        {
+            const auto multiply_by_ten = [](int& i) { i *= 10; };
+            list.for_each(multiply_by_ten);
+
+            THEN("They match the updated values")
+            {
+                const auto match_ten    = [](const int i) { return i == 10; };
+                const auto match_twenty = [](const int i) { return i == 20; };
+                const auto match_thirty = [](const int i) { return i == 30; };
+
+                const auto ten    = *list.find_first_if(match_ten);
+                const auto twenty = *list.find_first_if(match_twenty);
+                const auto thirty = *list.find_first_if(match_thirty);
+                CHECK(ten == 10);
+                CHECK(twenty == 20);
+                CHECK(thirty == 30);
+            }
+
+            AND_WHEN("We look for the original values")
+            {
+                const auto one   = list.find_first_if(TestList1Fixture::match_one);
+                const auto two   = list.find_first_if(TestList1Fixture::match_two);
+                const auto three = list.find_first_if(TestList1Fixture::match_three);
+
+                AND_THEN("We can't find them anymore")
+                {
+                    CHECK(one == nullptr);
+                    CHECK(two == nullptr);
+                    CHECK(three == nullptr);
+                }
+            }
+        }
+    }
+}
+
+TEST_CASE_METHOD(TestList1Fixture, "Removing elements", "[test][internal]")
+{
+    GIVEN("A list with known added elements")
+    {
+        List1<int> list;
+
+        list.push_front(1);
+        list.push_front(2);
+        list.push_front(3);
+
+        WHEN("We remove one of them")
+        {
+            list.remove_if(TestList1Fixture::match_two);
+
+            THEN("It cannot be found but the other ones still can")
+            {
+                const auto one   = list.find_first_if(TestList1Fixture::match_one);
+                const auto two   = list.find_first_if(TestList1Fixture::match_two);
+                const auto three = list.find_first_if(TestList1Fixture::match_three);
+
+                CHECK(*one == 1);
+                CHECK(two == nullptr);
+                CHECK(*three == 3);
+            }
+        }
     }
 }
