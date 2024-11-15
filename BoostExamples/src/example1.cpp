49b42cde4d217e67dc9c6d81df872edcdbcdec80
Loïc Corenthy <loic.corenthy@gmail.com>
2024-11-15 16:12:21 +0000

    Add asio tutorial 1

---
+++ BoostExamples/src/example1.cpp
@@ -0,0 +1,17 @@
+#define ASIO_STANDALONE
+#include <asio.hpp>
+#include <chrono>
+#include <iostream>
+
+int main()
+{
+    asio::io_context context;
+
+    asio::steady_timer timer(context, std::chrono::seconds(3));
+
+    timer.wait();
+
+    std::cout << "Hello asio!\n";
+
+    return 0;
+}
