354db998395bc7c679c4d0abd61179aa52e6bd29
Loïc Corenthy <loic.corenthy@gmail.com>
2024-11-15 22:19:44 +0000

    Add asio tutorial 3

---
+++ BoostExamples/src/example3.cpp
@@ -0,0 +1,33 @@
+#define ASIO_STANDALONE
+#include <asio.hpp>
+#include <iostream>
+#include <functional>
+
+void print(const asio::error_code& , asio::steady_timer* timer, int* count)
+{
+    if (*count < 5)
+    {
+        std::cout << "Count is  " << *count << '\n';
+        timer->expires_from_now(asio::chrono::seconds(1));
+        *count += 1;
+
+        timer->async_wait(std::bind(&print, asio::placeholders::error, timer, count));
+    }
+}
+
+int main()
+{
+    asio::io_context context;
+
+    asio::steady_timer timer(context, asio::chrono::seconds(1));
+    std::cout << "Start timer for 5 seconds\n";
+
+    int count = 0;
+    timer.async_wait(std::bind(&print, asio::placeholders::error, &timer, &count));
+
+    context.run();
+
+    std::cout << "Final count is " << count << '\n';
+
+    return 0;
+}
