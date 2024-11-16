77ce74888947d90f4d7e283bca3bf45c6b2f9763
Loïc Corenthy <loic.corenthy@gmail.com>
2024-11-16 07:22:16 +0000

    Add asio tutorial 4

---
+++ BoostTutorials/src/tutorial4.cpp
@@ -0,0 +1,48 @@
+#define ASIO_STANDALONE
+#include <asio.hpp>
+#include <iostream>
+#include <functional>
+
+class Printer
+{
+public:
+    Printer(asio::io_context& io)
+    :_io(io),
+     _timer(_io, asio::chrono::seconds(1))
+    {
+        _timer.async_wait(std::bind(&Printer::print, this));
+    }
+
+    void print()
+    {
+        if (_counter < 5)
+        {
+            std::cout << "Count is  " << _counter << '\n';
+            _timer.expires_from_now(asio::chrono::seconds(1));
+            _counter++;
+
+            _timer.async_wait(std::bind(&Printer::print, this));
+        }        
+    }
+
+    int counter() const
+    {
+        return _counter;
+    }
+
+private:
+    asio::io_context& _io;
+    asio::steady_timer _timer;
+    int _counter = 0;
+};
+
+int main()
+{
+    asio::io_context io;
+    Printer printer(io);
+    io.run();
+
+    std::cout << "The final count is " << printer.counter() << '\n';
+    return 0;
+}
+
