02bed9c1615264d126dd1e9160a33b0270f167e8
Loïc Corenthy <loic.corenthy@gmail.com>
2024-11-17 23:44:03 +0000

    Add asio tutorial 10

---
+++ BoostTutorials/src/tutorial10.cpp
@@ -0,0 +1,58 @@
+#define ASIO_STANDALONE
+#include <asio.hpp>
+
+#include <array>
+#include <ctime>
+#include <iostream>
+#include <string>
+
+using asio::ip::udp;
+
+using std::array;
+using std::cout;
+using std::endl;
+using std::string;
+using std::cin;
+
+string make_daytime()
+{
+    using namespace std;
+    time_t now = time(0);
+    return ctime(&now);
+}
+
+int main()
+{
+    try
+    {
+        asio::io_context io_ctx;
+
+        udp::socket socket(io_ctx, udp::endpoint(udp::v4(), 13));
+
+        bool run = true;
+        while (run)
+        {
+            array<char, 1> receive_buffer = {};
+            udp::endpoint  remote_endpoint;
+            socket.receive_from(asio::buffer(receive_buffer), remote_endpoint);
+
+            cout << "Received connection request from " << remote_endpoint.address().to_string() << endl;
+
+            string message = make_daytime();
+
+            socket.send_to(asio::buffer(message), remote_endpoint);
+
+            cout << "Keep running?\n";
+            char answer;
+            cin >> answer;
+
+            run = answer == 'y';
+        }
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    return EXIT_SUCCESS;
+}
